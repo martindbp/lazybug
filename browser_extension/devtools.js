d3daa123e0fe92a4489ee06381febdc535e1d2e8
Martin Pettersson <me@martindbp.com>
2022-01-20 13:54:06 +0100

    Allow measuring tool to be started with key press

--- browser_extension/devtools.js
+++ browser_extension/devtools.js
@@ -11,51 +11,87 @@ chrome.runtime.onMessage.addListener(msgObj => {
             event.stopImmediatePropagation();
         }, { once: true });
 
-        window.addEventListener("mousedown", (downEvent) => {
-            downEvent.preventDefault();
-            downEvent.stopPropagation();
-            if (AVElement === null) {
-                AVElement = document.querySelector("video");
-            }
+        if (AVElement === null) {
+            AVElement = document.querySelector("video");
+        }
 
-            const measureDiv = document.createElement("div");
+        let clientX = null;
+        let clientY = null;
+        let mouseDownClientY = null;
+        let measureDiv = null;
+        let moveHandler = null;
+        window.addEventListener("mousemove", function(event) {
+            clientX = event.clientX;
+            clientY = event.clientY;
+        });
+
+        function stopMeasuring() {
+            console.log('Stop measuring');
+            var videoRect = AVElement.getBoundingClientRect();
+            var videoTop = Math.round(AVElement.videoHeight * (mouseDownClientY - videoRect.top) / videoRect.height);
+            var videoBottom = Math.round(AVElement.videoHeight * (clientY - videoRect.top) / videoRect.height);
+            captionBottom = videoBottom / AVElement.videoHeight;
+            captionTop = videoTop / AVElement.videoHeight;
+            measureDiv.remove();
+            measureDiv = null;
+            window.removeEventListener("mousemove", moveHandler);
+            moveHandler = null;
+            mouseDownClientY = null;
+
+            const data = {
+                "ocr_params": [
+                    {
+                        "type": "hanzi",
+                        "caption_top": captionTop,
+                        "caption_bottom": captionBottom,
+                        "start_time": 0
+                    }
+                ]
+            };
+            prompt('Copy:', JSON.stringify(data, null, 2));
+        }
+
+        function startMeasuring() {
+            console.log('Start measuring');
+            measureDiv = document.createElement("div");
             measureDiv.style.cssText = "position:absolute; color: white; border-color: black; border: 2px solid black;";
             document.body.appendChild(measureDiv);
 
-            measureDiv.style.left = downEvent.clientX + "px";
-            measureDiv.style.top = downEvent.clientY + "px";
+            const mouseDownClientX = clientX;
+            mouseDownClientY = clientY;
+            measureDiv.style.left = clientX + "px";
+            measureDiv.style.top = clientY + "px";
             measureDiv.style.width = 0 + "px";
             measureDiv.style.height = 0 + "px";
 
-            const moveHandler = (moveEvent) => {
-                measureDiv.style.width = (moveEvent.clientX - downEvent.clientX) + "px";
-                measureDiv.style.height = (moveEvent.clientY - downEvent.clientY) + "px";
+            moveHandler = (moveEvent) => {
+                measureDiv.style.width = (moveEvent.clientX - mouseDownClientX) + "px";
+                measureDiv.style.height = (moveEvent.clientY - mouseDownClientY) + "px";
             };
 
             window.addEventListener("mousemove", moveHandler);
             window.addEventListener("mouseup", (upEvent) => {
                 upEvent.preventDefault();
                 upEvent.stopPropagation();
-                var videoRect = AVElement.getBoundingClientRect();
-                var videoTop = Math.round(AVElement.videoHeight * (downEvent.clientY - videoRect.top) / videoRect.height);
-                var videoBottom = Math.round(AVElement.videoHeight * (upEvent.clientY - videoRect.top) / videoRect.height);
-                captionBottom = videoBottom / AVElement.videoHeight;
-                captionTop = videoTop / AVElement.videoHeight;
-                measureDiv.remove();
-                window.removeEventListener("mousemove", moveHandler);
-
-                const data = {
-                    "ocr_params": [
-                        {
-                            "type": "hanzi",
-                            "caption_top": captionTop,
-                            "caption_bottom": captionBottom,
-                            "start_time": 0
-                        }
-                    ]
-                };
-                prompt('Copy:', JSON.stringify(data, null, 2));
+                stopMeasuring();
             }, { once: true });
+            window.addEventListener("keyup", (keyUpEvent) => {
+                keyUpEvent.preventDefault();
+                keyUpEvent.stopPropagation();
+                stopMeasuring();
+            }, { once: true });
+        }
+
+        window.addEventListener("mousedown", (downEvent) => {
+            downEvent.preventDefault();
+            downEvent.stopPropagation();
+            startMeasuring();
+        }, { once: true });
+
+        window.addEventListener("keydown", (keyDownEvent) => {
+            keyDownEvent.preventDefault();
+            keyDownEvent.stopPropagation();
+            startMeasuring();
         }, { once: true });
     }
     else if (msgObj === 'printplaylist') {
