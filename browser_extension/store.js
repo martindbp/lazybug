8b7552332fe0ec05e75f0cb5b257360023fd811b
Martin Pettersson <me@martindbp.com>
2022-01-27 13:35:53 +0100

    Sync knowledge and options to local storage

--- browser_extension/store.js
+++ browser_extension/store.js
@@ -2,6 +2,9 @@ const KnowledgeUnknown = 0;
 const KnowledgeKnown = 1;
 const KnowledgeLearning = 2;
 
+let knowledgeChanged = false;
+let optionsChanged = false;
+
 const store = new Vuex.Store({
     state: {
         knowledge: Vue.ref({}),
@@ -26,14 +29,19 @@ const store = new Vuex.Store({
         setShowDictionary(state, val) {
             state.showDictionary = val;
         },
+        setKnowledge(state, knowledge) {
+            state.knowledge = knowledge;
+        },
         setKnowledgeKey(state, keyVal) {
             state.knowledge[keyVal.key] = keyVal.val;
+            knowledgeChanged = true;
         },
         setKnowledgeKeys(state, keysVals) {
             for (let i = 0; i < keysVals.keys.length; i++) {
                 const keyVal = {key: keysVals.keys[i], val: keysVals.vals[i]};
                 state.knowledge[keyVal.key] = keyVal.val;
             }
+            knowledgeChanged = true;
         },
         increaseCaptionFontScale(state) {
             state.captionFontScale = Math.min(state.captionFontScale + 0.1, 1.0);
@@ -55,11 +63,16 @@ const store = new Vuex.Store({
         setPeekStates(state, val) {
             state.peekStates = val;
         },
+        setOptions(state, options) {
+            state.options = options;
+        },
         setOption(state, option) {
             state.options[option.key] = option.value;
+            optionsChanged = true;
         },
         setKnownLevel(state, val) {
             state.options.knownLevels[val.type] = val.level;
+            optionsChanged = true;
         },
     },
     getters: {
@@ -71,3 +84,22 @@ const store = new Vuex.Store({
     }
 })
 
+// Read current knowledge/options
+chrome.storage.local.get(['knowledge', 'options'], (items) => {
+    if (items.knowledge) store.commit('setKnowledge', items.knowledge);
+    if (items.options) store.commit('setOptions', items.options);
+});
+
+
+setInterval(function() {
+    if (knowledgeChanged) {
+        chrome.storage.local.set({knowledge: store.state.knowledge}, function() {
+            knowledgeChanged = false;
+        });
+    }
+    if (optionsChanged) {
+        chrome.storage.local.set({options: store.state.options}, function() {
+            optionsChanged = false;
+        });
+    }
+}, 5000);
