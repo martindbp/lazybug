cc17964287590e05a8a73d2cdb18466c498297e1
Martin Pettersson <me@martindbp.com>
2022-02-25 12:50:02 +0100

    Move good segmentation mining to fix_raw_caption_errors script

--- fix_raw_caption_errors.py
+++ fix_raw_caption_errors.py
@@ -7,14 +7,39 @@ import argparse
 import cv2
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
+from pymatting import cutout, blend
 
 from wrapped_json import json
 
 parser = argparse.ArgumentParser(description='Fix raw caption errors')
 parser.add_argument('filename', type=str)
+parser.add_argument('--show', type=str, default='')
+parser.add_argument('--keys', action='store_true')
 parser.add_argument('--num', type=int, default=30)
+parser.add_argument('--dilate-foreground', type=int, default=2)
+parser.add_argument('--dilate-move-foreground', type=str, default='')
+parser.add_argument('--erode-foreground', type=int, default=0)
+parser.add_argument('--dilate-unknown', type=int, default=3)
 
 args = parser.parse_args()
+
+keys = {
+    'e': 'edit',
+    'j': 'join lines',
+    's': 'save edited raw captions',
+    'backspace': 'prev',
+    'space': 'next',
+    'd': 'half page next',
+    'u': 'half page prev',
+    'g': 'save good example'
+}
+
+
+if args.keys:
+    print('Keys:')
+    for key, desc in keys.items():
+        print(key, ': ', desc)
+    sys.exit()
 
 orig_filename = args.filename[:-5] + '-orig.json'
 
@@ -29,6 +54,7 @@ lines = raw_captions['lines']
 
 curr_show_idx = 0
 
+CAPTION_DATA_DIR = 'data/remote/private/caption_data'
 INPUT_HEIGHT = 80
 SCALE_FACTOR = 2
 LINE_HEIGHT = INPUT_HEIGHT // SCALE_FACTOR
@@ -36,8 +62,26 @@ LINE_WIDTH_BUFFER = 5
 FONT_PATH = "./data/remote/private/fonts/fangzhengheiti.ttf"
 FONT_BUFFER = 20
 FONT = ImageFont.truetype(FONT_PATH, (INPUT_HEIGHT-2*FONT_BUFFER) // SCALE_FACTOR)
+img_path = '/tmp/cutout_img.png'
+trimap_path = '/tmp/cutout_trimap.png'
+out_path = '/tmp/cutout_out.png'
+cutout_dir = f'{CAPTION_DATA_DIR}/cutouts/{args.show}/'
+os.makedirs(cutout_dir, exist_ok=True)
 
 selected_indices = set()
+
+if args.show != '':
+    empty_hashes = []
+    with open(f'{CAPTION_DATA_DIR}/cutouts/{args.show}.json', 'r') as f:
+        print('Read current background hashes file')
+        empty_hashes = json.load(f)
+
+    for line in lines:
+        if line[0] == '' and os.path.exists(f'{CAPTION_DATA_DIR}/images/{line[-1]}.jpg'):
+            empty_hashes.append(line[-1])
+    with open(f'{CAPTION_DATA_DIR}/cutouts/{args.show}.json', 'w') as f:
+        print('Writing background hashes file')
+        json.dump(list(set(empty_hashes)), f)
 
 def draw_frame():
     height = args.num * LINE_HEIGHT
@@ -57,8 +101,8 @@ def draw_frame():
 
     for i, line in enumerate(lines[curr_show_idx:curr_show_idx+args.num]):
         hash = line[-1]
-        img = cv2.imread(f'data/remote/private/caption_data/images/{hash}.jpg')
-        seg = cv2.imread(f'data/remote/private/caption_data/segmentation_probs/{hash}.png')
+        img = cv2.imread(f'{CAPTION_DATA_DIR}/images/{hash}.jpg')
+        seg = cv2.imread(f'{CAPTION_DATA_DIR}/segmentation_probs/{hash}.png')
         if img is None:
             continue
         if seg is None:
@@ -172,3 +216,70 @@ while True:
     elif key == 32:  # space
         selected_indices = set()
         curr_show_idx = min(curr_show_idx + args.num, len(lines) - 1)
+    elif key == ord('g'):
+        if args.show == '':
+            print('ERROR: need to set --show name for saving good examples')
+            sys.exit(1)
+
+        for idx in selected_indices:
+            idx = idx + curr_show_idx
+            line = lines[idx]
+            hash = line[-1]
+            seg_prob_file = f'{CAPTION_DATA_DIR}/segmentation_probs/{hash}.png'
+            img_file = f'{CAPTION_DATA_DIR}/images/{hash}.jpg'
+            seg = cv2.imread(seg_prob_file, cv2.IMREAD_GRAYSCALE)
+            img = cv2.imread(img_file)
+            if seg is None:
+                last_empty = line
+                continue
+
+            foreground = 255*(seg > 255/2).astype('uint8')
+
+            if args.dilate_foreground > 0:
+                foreground = cv2.dilate(foreground, np.ones((3, 3), np.uint8), iterations=args.dilate_foreground)
+
+            if args.erode_foreground > 0:
+                foreground = cv2.erode(foreground, np.ones((3, 3), np.uint8), iterations=args.dilate_foreground)
+
+            if args.dilate_move_foreground:
+                dy, dx = args.dilate_move_foreground.split(',')
+                dy, dx = int(dy), int(dx)
+                foreground = foreground | np.roll(foreground.copy(), (dy, dx), axis=(0, 1))
+
+            if args.dilate_unknown > 0:
+                unknown = cv2.dilate(foreground.copy(), np.ones((3, 3), np.uint8), iterations=args.dilate_unknown)
+                unknown = unknown & ~foreground
+
+                trimap = foreground.copy()
+                trimap[unknown > 0] = 255 // 2
+
+                img_plus_trimap = img.copy()
+                img_plus_trimap[..., -1] = trimap
+                cv2.imshow('img_plus_trimap', img_plus_trimap)
+
+                cv2.imwrite(img_path, img)
+                cv2.imwrite(trimap_path, trimap)
+                cutout(img_path, trimap_path, out_path)
+                cutout_img = cv2.imread(out_path, cv2.IMREAD_UNCHANGED)
+                foreground_img = cutout_img[..., :3].astype('float') / 255
+                alpha = cutout_img[..., -1].astype('float') / 255
+                alpha = cv2.blur(alpha, (3, 3))
+                cv2.imshow('cutout_img', cutout_img)
+            else:
+                alpha = seg.astype('float') / 255
+                foreground_img = img.astype('float') / 255
+                cutout_img = np.dstack((img, seg))
+
+
+            white_background = np.ones_like(foreground_img, 'float')
+            black_background = np.zeros_like(foreground_img, 'float')
+            blend_white = (255 * blend(foreground_img, white_background, alpha)).astype('uint8')
+            blend_black = (255 * blend(foreground_img, black_background, alpha)).astype('uint8')
+
+            cv2.imshow("blend black", blend_black)
+            cv2.imshow("blend white", blend_white)
+
+            save_path = f'{cutout_dir}{hash}.png'
+            print('Saving to', save_path)
+            cv2.imwrite(save_path, cutout_img)
+            cv2.waitKey(1)
