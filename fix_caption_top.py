80a55fcf2b5d375875ebeb88ad06bac8e6b3334e
Martin Pettersson <me@martindbp.com>
2022-03-02 09:29:49 +0100

    Change fix caption top script to make bounding rects global

--- fix_caption_top.py
+++ fix_caption_top.py
@@ -2,16 +2,35 @@ import os
 from wrapped_json import json
 import glob
 
-for filename in glob.glob('data/remote/private/caption_data/raw_captions/*.json'):
+files = (
+    #glob.glob('data/remote/private/caption_data/raw_captions/*.json') +
+    glob.glob('data/remote/public/subtitles/*.json')
+)
+
+for filename in files:
     if filename.endswith('merkl'):
         continue
 
     with open(filename, 'r') as f:
         data = json.loads(f.read())
 
-    if not isinstance(data['caption_top'], float):
-        data['caption_top'] /= data['frame_size'][0]
-        print(data['caption_top'])
+    if 'frame_size' not in data or data['frame_size'] is None:
+        continue
 
-    with open(filename, 'w') as f:
-        json.dump(data, f)
+    y_offset = data['caption_top'] * data['frame_size'][0]
+    for line in data['lines']:
+        rects = line[3] if isinstance(line[3][0], list) else [line[3]]
+
+        for rect in rects:
+            if rect is None or rect[0] is None:
+                continue
+            print('Prev rect', rect)
+
+            rect[2] += y_offset
+            rect[3] += y_offset
+            rect[2] = round(rect[2])
+            rect[3] = round(rect[3])
+            print('After rect', rect)
+
+    #with open(filename, 'w') as f:
+        #json.dump(data, f)
