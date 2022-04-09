b5551b3f12d864b40571532e1d399d58de2023d3
Martin Pettersson <me@martindbp.com>
2022-04-09 09:51:17 +0200

    Add script to remove unnecessary dev stuff from manifest.json

---
+++ make_release_manifest.py
@@ -0,0 +1,17 @@
+import sys
+from wrapped_json import json
+
+manifest_file = sys.argv[1]
+
+with open(manifest_file, 'r') as f:
+    data = json.load(f)
+    localhost_idx = data['host_permissions'].index('http://localhost/*')
+    del data['host_permissions'][localhost_idx]
+
+    file_idx = data['content_scripts'][0]['matches'].index('file:///*')
+    del data['content_scripts'][0]['matches'][file_idx]
+
+    # Delete deepl stuff
+    del data['content_scripts'][1]
+
+    print(json.dumps(data))
