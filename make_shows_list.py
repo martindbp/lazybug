fade111be09f3c1b205922dcc19492f004cb53a9
Martin Pettersson <me@martindbp.com>
2022-06-12 18:19:24 +0200

    Fail show list generation if name is not present

--- make_shows_list.py
+++ make_shows_list.py
@@ -1,5 +1,6 @@
-from wrapped_json import json
+import sys
 import glob
+from wrapped_json import json
 
 released_shows = {}
 for filename in glob.glob('data/remote/public/shows/*.json'):
@@ -7,6 +8,9 @@ for filename in glob.glob('data/remote/public/shows/*.json'):
         show_name = filename.split('/')[-1].split('.')[0]
         show = json.load(f)
         released_shows[show_name] = show
+        if show.get('released', False) and not show['name'] or (isinstance(show['name'], dict) and (not show['name']['hz'] or not show['name']['py'] or not show['name']['en'])):
+            print('ERROR No name for show:', show_name)
+            sys.exit(1)
 
 with open('data/remote/public/show_list.json', 'w') as f:
     json.dump(released_shows, f)
