1f4b2d7071efa5c2b32a5d277f76023c61578998
Martin Pettersson <me@martindbp.com>
2022-08-27 14:42:52 +0200

    Add ssl cert files

--- backend/main.py
+++ backend/main.py
@@ -4,5 +4,19 @@ import uvicorn
 port = sys.argv[1] if len(sys.argv) > 1 else '8000'
 port = int(port)
 
+ssl_keyfile = None
+ssl_certfile = None
+if port == 80:
+    # Need to link to ssl files
+    ssl_keyfile = 'privkey.pem'
+    ssl_certfile = 'fullchain.pem'
+
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run(
+        "app.app:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="info",
+        ssl_keyfile=ssl_keyfile,
+        ssl_certfile=ssl_certfile,
+    )
