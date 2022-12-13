7f0cc69bc96a237a2e1a1efb70e3bca46c29e1f3
Martin Pettersson <me@martindbp.com>
2022-12-13 22:12:48 +0100

    Fix issue with host address on ssl

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,16 @@ import os
 import sys
 import uvicorn
 
+LOCAL_ONLY = os.getenv('LOCAL_ONLY') is not None
+
 port = sys.argv[1] if len(sys.argv) > 1 else '8000'
 port = int(port)
 
 ssl_keyfile = None
 ssl_certfile = None
-if port == 443:
+is_ssl = port == 443
+host = '127.0.0.1' if LOCAL_ONLY or not is_ssl else '0.0.0.0'  # NOTE: need 127... for localhost.ext cert
+if is_ssl:
     # Need to link to ssl files
     ssl_keyfile = 'data/local/ssl_keys/privkey.pem'
     ssl_certfile = 'data/local/ssl_keys/fullchain.pem'
@@ -18,7 +22,7 @@ if port == 443:
 if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
-        host="127.0.0.1",
+        host=host,
         port=port,
         log_level="info",
         ssl_keyfile=ssl_keyfile,
